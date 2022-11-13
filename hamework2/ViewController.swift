f45893b0d47ceb0726cd268ceedfa4a3b0609aaf
vanessa12101 <choumina838@gmail.com>
2022-11-13 23:17:28 +0800

    Initial Commit

---
+++ hamework2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hamework2
+//
+//  Created by 陳家堯 on 2022/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
